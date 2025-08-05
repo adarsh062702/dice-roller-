0530e6d51a47b0bf9ecc87f180ccd2d72af5a88f
Adarsh <bindal890@gmail.com>
2025-08-06 04:42:35 +0530

    Add files via upload

---
+++ Adarsh.java
@@ -0,0 +1,100 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Adarsh {
+
+    public static void main(String[] args){
+
+        //dice roll program
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String playAgain = "yes";
+        do {
+            int numOfDice;
+            int total = 0;
+
+            System.out.print("How many dice would you like to roll: ");
+            numOfDice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (numOfDice > 0){
+                for (int i = 0; i < numOfDice; i++){
+                    int roll = random.nextInt(1 , 7);
+                    dice(roll);
+                    System.out.println("You rolled a " + roll);
+                    total += roll;
+                }
+                System.out.println("Total: " + total);
+            }
+            else {
+                System.out.println("Number of dice must be more than zero!");
+            }
+
+            System.out.print("Roll again? (yes/no): ");
+            playAgain = scanner.nextLine().toLowerCase();
+
+        }while (playAgain.equals("yes"));
+
+        System.out.println("Thank you for playing");
+
+    }
+
+    //dice roll method
+    static void dice(int roll){
+        String dice1 =  """
+                 ---------
+                |         |
+                |    •    |
+                |         |
+                 ---------
+                """;
+        String dice2 =  """
+                 ---------
+                |  •      |
+                |         |
+                |      •  |
+                 ---------
+                """;
+        String dice3 =  """
+                 ---------
+                |  •      |
+                |    •    |
+                |      •  |
+                 ---------
+                """;
+        String dice4 =  """
+                 ---------
+                |  •   •  |
+                |         |
+                |  •   •  |
+                 ---------
+                """;
+        String dice5 =  """
+                 ---------
+                |  •   •  |
+                |    •    |
+                |  •   •  |
+                 ---------
+                """;
+        String dice6 =  """
+                 ---------
+                |  •   •  |
+                |  •   •  |
+                |  •   •  |
+                 ---------
+                """;
+
+        switch (roll){
+            case 1 -> System.out.print(dice1);
+            case 2 -> System.out.print(dice2);
+            case 3 -> System.out.print(dice3);
+            case 4 -> System.out.print(dice4);
+            case 5 -> System.out.print(dice5);
+            case 6 -> System.out.print(dice6);
+            default -> System.out.println("not valid!");
+        }
+
+    }
+
+}
